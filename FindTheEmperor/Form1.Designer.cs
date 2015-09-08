395545de67e0dbc68bc227ac3e579f1294d8d1e5
yanglichun <646490547@qq.com>
2015-09-08 13:36:04 +0800

    first commit

---
+++ FindTheEmperor/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Dynasty = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.huangdi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.start = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.end = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.shihao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emperorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emperorBindingSource)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "点击查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "输入年份",
+            "输入皇帝名称",
+            "输入朝代"});
+            this.comboBox1.Location = new System.Drawing.Point(174, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(98, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "输入年份";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "输入";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Dynasty,
+            this.huangdi,
+            this.start,
+            this.end,
+            this.shihao});
+            this.dataGridView1.Location = new System.Drawing.Point(10, 137);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(702, 369);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Dynasty
+            // 
+            this.Dynasty.DataPropertyName = "Dynasty";
+            this.Dynasty.HeaderText = "朝代";
+            this.Dynasty.Name = "Dynasty";
+            this.Dynasty.Width = 183;
+            // 
+            // huangdi
+            // 
+            this.huangdi.DataPropertyName = "huangdi";
+            this.huangdi.HeaderText = "皇帝";
+            this.huangdi.Name = "huangdi";
+            this.huangdi.Width = 153;
+            // 
+            // start
+            // 
+            this.start.DataPropertyName = "start";
+            this.start.HeaderText = "登基";
+            this.start.Name = "start";
+            this.start.Width = 55;
+            // 
+            // end
+            // 
+            this.end.DataPropertyName = "end";
+            this.end.HeaderText = "退位";
+            this.end.Name = "end";
+            this.end.Width = 55;
+            // 
+            // shihao
+            // 
+            this.shihao.DataPropertyName = "shihao";
+            this.shihao.HeaderText = "谥号";
+            this.shihao.Name = "shihao";
+            this.shihao.Width = 213;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 509);
+            this.panel1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(741, 533);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "三皇五帝";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emperorBindingSource)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource emperorBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dynastyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn shihaoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn startDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn endDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nianhaoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dynasty;
+        private System.Windows.Forms.DataGridViewTextBoxColumn huangdi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn start;
+        private System.Windows.Forms.DataGridViewTextBoxColumn end;
+        private System.Windows.Forms.DataGridViewTextBoxColumn shihao;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ FindTheEmperor/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        TextBox t = new TextBox();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string byYear(int year) 
+        {
+
+            string connstr = "SELECT Dynasty,Name as huangdi,start,[end],shihao FROM Emperor WHERE Start <= " + year + " AND [End] >= " + year;
+           return connstr;
+        }
+
+        public string byName(string name)
+        {
+
+            string connstr = "SELECT Dynasty,Name as huangdi,start,[end],shihao FROM Emperor WHERE name like '%" + name + "%' ORDER BY Start ASC";
+            return connstr;
+        }
+        public string byDynasty(string dyn) 
+        {
+            string connstr = "SELECT Dynasty,Name as huangdi,start,[end],shihao FROM Emperor WHERE Dynasty like '%" + dyn + "%'";
+            return connstr;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string conn1 = "Data Source=YANGLICHUN;Initial Catalog=Emperor;user id=sa;password=19930405;Integrated Security=True";
+            
+            SqlConnection conn = new SqlConnection(conn1);
+            DataSet ds = new DataSet();
+            string t1 =this.textBox1.Text;
+          if (comboBox1.SelectedIndex == 0)
+         {
+             try
+             {
+                 int t2 = Convert.ToInt32(t1);
+                 if (t2 < -221 || t2 > 1912)
+                 {
+                     MessageBox.Show("请输入正确的数字！");
+                 }
+                 SqlDataAdapter sda=new SqlDataAdapter(byYear(t2),conn);
+                 sda.Fill(ds);
+                 
+             }
+             catch {
+                 MessageBox.Show("请输入正确的数字！");
+                 return;
+             }
+          }
+          else if (comboBox1.SelectedIndex == 1)
+          {
+              SqlDataAdapter sda = new SqlDataAdapter(byName(t1), conn);
+              sda.Fill(ds);
+          }
+          else if (comboBox1.SelectedIndex == 2)
+          {
+              SqlDataAdapter sda = new SqlDataAdapter(byDynasty(t1), conn);
+              sda.Fill(ds); 
+            }
+            
+            //conn.Open();
+          dataGridView1.DataSource = ds.Tables[0];
+            //listBox1.Items.Add("朝代\n姓名\n谥号\n生\n死\n登基\n退位\n年号\n介绍");
+            //if (dr.HasRows)
+            //{
+            //    while (dr.Read())
+            //    {
+            //        //if(dr[6].ToString()||dr[7].ToString()!=null)
+            //        try
+            //        {
+            //            int s = 1;
+            //            int sev = Convert.ToInt32(dr[7]);
+            //            int six = Convert.ToInt32(dr[6]);
+            //            int r = sev - six;
+            //            //MessageBox.Show(r.ToString());
+            //            if (r == 0)
+            //            {
+            //                s = 1;
+            //            }
+            //            else
+            //            {
+            //                s = (Convert.ToInt32(dr[7].ToString()) - Convert.ToInt32(dr[6].ToString()));
+            //            }
+            //            //MessageBox.Show(dr[0] + dr[2].ToString() + dr[3] + dr[4] + dr[5]);
+            //            //string s= dr[9].ToString() + dr[1]+ dr[2] + dr[3] + dr[4] + dr[5]+dr[6]+dr[7]+dr[8];
+            //            listBox1.Items.Add(string.Format("{0}\t皇帝:{1}\t登基:{2}年\t退位:{3}年\t在位约:{4}年",
+            //            dr[1].ToString(),
+            //            dr[2].ToString(),
+            //            dr[6].ToString(),
+            //            dr[7].ToString(), s.ToString()));
+            //        }
+            //        catch
+            //        {
+            //            listBox1.Items.Add(string.Format("{0}\t皇帝:{1}\t登基:xx{2}年\t退位:xx{3}年\t在位约:0年",
+            //                dr[1].ToString(),
+            //                dr[2].ToString(),
+            //                dr[6].ToString(),
+            //                dr[7].ToString()));
+            //        }
+            //    }
+            //}
+            //else { MessageBox.Show("没有找到噢~~~"); }
+            ////try
+            ////{
+            ////    MessageBox.Show(cmd.ExecuteScalar().ToString());
+            ////}
+            ////catch { MessageBox.Show("额~暂时没找到哦- -"); }
+            //conn.Close();
+        }
+
+       
+    }
+}
